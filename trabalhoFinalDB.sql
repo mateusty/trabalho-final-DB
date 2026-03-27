8a3dde3d9b991032a37272dffd3563e3068cf2c8
Mateus Tamaki <mateustamaki43@gmail.com>
2026-03-26 22:49:01 -0300

    Adicionando arquivo SQL

---
+++ trabalhoFinalDB.sql
@@ -0,0 +1,9 @@
+-- Criação de Banco de dados / Tabelas (todas as tipos, restrições e chaves).	Emanuel
+
+-- Inserção de valores e Update.	Liliane, Caroline
+
+-- Índices e Delete.	Arthur
+
+-- Consultas.	Mateus
+
+-- Modelos Conceitual (Lógico e Físico).	Letícia
